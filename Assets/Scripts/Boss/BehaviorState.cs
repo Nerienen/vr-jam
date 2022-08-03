0eaa89435be12400ee24848c3e2325c2f3a4dccd
Jorge Blanco <jbsfreelance@gmail.com>
2022-08-02 21:02:07 -0700

    Added a simple state machine to the boss locomotion to switch between pursuing the player, searching for the player and patrolling.

---
+++ Assets/Scripts/Boss/BehaviorState.cs
@@ -0,0 +1,11 @@
+﻿namespace Boss
+{
+    public enum BehaviorState
+    {
+        Idle = 0,
+        PlayerFound = 10,
+        Attacking = 20,
+        PlayerLost = 30,
+        Stunned = 40,
+    }
+}
---
+++ Assets/Scripts/Boss/Boss.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Boss
+{
+    public class Boss : MonoBehaviour
+    {
+        public BehaviorState state;
+
+        private void Start()
+        {
+            state = BehaviorState.Idle;
+        }
+    }
+}
